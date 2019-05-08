a0465c6e78d03571fff4be73d550bf5ce5cd5fcd
Rajat Sarkari <rsarkari@xilinx.com>
2019-05-08 11:25:23 +0530

    sdx to scout change

--- common/includes/opencv/opencv.mk
+++ common/includes/opencv/opencv.mk
@@ -1,2 +1,2 @@
-opencv_CXXFLAGS=-I$(XILINX_SDX)/include
-opencv_LDFLAGS=-L$(XILINX_SDX)/lnx64/tools/opencv/ -lopencv_core -lopencv_highgui -lopencv_imgproc
+opencv_CXXFLAGS=-I$(XILINX_SCOUT)/include
+opencv_LDFLAGS=-L$(XILINX_SCOUT)/lnx64/tools/opencv/ -lopencv_core -lopencv_highgui -lopencv_imgproc
