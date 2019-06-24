d831ce3f5af5f3221831b769d69156dbb0a1abe8
Rajat Sarkari <rsarkari@xilinx.com>
2019-06-24 10:27:55 +0530

    common update

--- common/includes/oclHelper/oclHelper.mk
+++ common/includes/oclHelper/oclHelper.mk
@@ -1,3 +1,3 @@
-oclHelper_SRCS:=${COMMON_REPO}/includes/oclHelper/oclHelper.cpp ${COMMON_REPO}/includes/oclHelper/oclErrorCodes.cpp
-oclHelper_HDRS:=${COMMON_REPO}/includes/oclHelper/oclHelper.h
-oclHelper_CXXFLAGS:=-I${COMMON_REPO}/includes/oclHelper
+oclHelper_SRCS:=${COMMON_REPO}/common/includes/oclHelper/oclHelper.cpp ${COMMON_REPO}/common/includes/oclHelper/oclErrorCodes.cpp
+oclHelper_HDRS:=${COMMON_REPO}/common/includes/oclHelper/oclHelper.h
+oclHelper_CXXFLAGS:=-I${COMMON_REPO}/common/includes/oclHelper
