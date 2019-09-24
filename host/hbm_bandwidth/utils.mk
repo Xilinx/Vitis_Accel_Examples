cf09dc68a80e0feba7b935fae1a070090a5d14e6
Rajat Sarkari <rsarkari@xilinx.com>
2019-09-24 19:59:09 +0530

    xcpp updated to g++

--- host/hbm_bandwidth/utils.mk
+++ host/hbm_bandwidth/utils.mk
@@ -47,16 +47,16 @@ endif
 ifeq ($(HOST_ARCH), x86)
 ifneq ($(shell expr $(shell g++ -dumpversion) \>= 5), 1)
 ifndef XILINX_VIVADO
-	$(error [ERROR]: g++ version older. Please use 5.0 or above.)
+$(error [ERROR]: g++ version older. Please use 5.0 or above.)
 else
-	CXX := $(XILINX_VIVADO)/tps/lnx64/gcc-6.2.0/bin/g++
-	$(warning [WARNING]: g++ version older. Using g++ provided by the tool : $(CXX))
+CXX := $(XILINX_VIVADO)/tps/lnx64/gcc-6.2.0/bin/g++
+$(warning [WARNING]: g++ version older. Using g++ provided by the tool : $(CXX))
 endif
 endif
 else ifeq ($(HOST_ARCH), aarch64)
-	CXX := $(XILINX_VITIS)/gnu/aarch64/lin/aarch64-linux/bin/aarch64-linux-gnu-g++
+CXX := $(XILINX_VITIS)/gnu/aarch64/lin/aarch64-linux/bin/aarch64-linux-gnu-g++
 else ifeq ($(HOST_ARCH), aarch32)
-	CXX := $(XILINX_VITIS)/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/arm-linux-gnueabihf-g++
+CXX := $(XILINX_VITIS)/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/arm-linux-gnueabihf-g++
 endif
 
 check-devices:
