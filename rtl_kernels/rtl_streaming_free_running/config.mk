ad55acbe8541a7216405ef2c9f07a19607e15529
Rajat Sarkari <rajat.sarkari@xilinx.com>
2019-10-14 12:52:02 +0530

    rtl_streaming_k2k is fixed with multiple platform found error

--- rtl_kernels/rtl_streaming_free_running/config.mk
+++ rtl_kernels/rtl_streaming_free_running/config.mk
@@ -1,4 +1,4 @@
 VIVADO := $(XILINX_VIVADO)/bin/vivado
 $(TEMP_DIR)/myadder1.xo: 
 	mkdir -p $(TEMP_DIR)
-	$(VIVADO) -mode batch -source ./src/gen_xo.tcl -tclargs $(TEMP_DIR)/myadder1.xo myadder1 $(TARGET) $(DEVICE) $(XSA)
+	$(VIVADO) -mode batch -source ./src/gen_xo.tcl -tclargs $(TEMP_DIR)/myadder1.xo myadder1 $(TARGET) $(B_NAME)/$(XSA).xpfm $(XSA)
