ed398844c68c4e4610cf04d645d7ec350e33ecce
Adarsh Kapil <adarsh.kapil@xilinx.com>
2019-11-11 15:05:11 +0530

    New example device only buffer

---
+++ host/device_only_buffer/README.md
@@ -0,0 +1,39 @@
+Device Only Buffer (C)
+======================
+
+This example will demonstrate how to create buffers in global memory which are not mapped to host.
+
+***KEY CONCEPTS:*** Device only buffer
+
+***KEYWORDS:*** CL_MEM_HOST_NO_ACCESS
+
+## SUPPORTED SHELLS
+Shell | Board             | Software Version
+---------|-------------------|-----------------
+xilinx_u200_qdma|Xilinx Alveo U200|VITIS 2019.2
+xilinx_u200_xdma|Xilinx Alveo U200|VITIS 2019.2
+xilinx_u250_qdma|Xilinx Alveo U250|VITIS 2019.2
+xilinx_u250_xdma|Xilinx Alveo U250|VITIS 2019.2
+xilinx_u280_xdma|Xilinx Alveo U280|VITIS 2019.2
+xilinx_u50_xdma|Xilinx Alveo U50|VITIS 2019.2
+zc702_base|Xilinx Zynq-7000 SoC ZC702|VITIS 2019.2
+zc706_base|Xilinx Zynq-7000 SoC ZC706|VITIS 2019.2
+zcu102_base|Xilinx UltraScale+ MPSoC ZCU102|VITIS 2019.2
+zcu104_base|Xilinx UltraScale+ MPSoC ZCU104|VITIS 2019.2
+
+
+##  DESIGN FILES
+Application code is located in the src directory. Accelerator binary files will be compiled to the xclbin directory. The xclbin directory is required by the Makefile and its contents will be filled during compilation. A listing of all the files in this example is shown below
+
+```
+src/host.cpp
+src/madd.cpp
+src/mmult.cpp
+```
+
+##  COMMAND LINE ARGUMENTS
+Once the environment has been configured, the application can be executed by
+```
+./device_only_buf <device_only_buf XCLBIN>
+```
+
---
+++ host/device_only_buffer/src/mmult.cpp
@@ -0,0 +1,124 @@
+/**********
+Copyright (c) 2018, Xilinx, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********/
+
+
+#include <stdio.h>
+#include <string.h>
+
+//Maximum Array Size
+#define MAX_SIZE 32
+
+//TRIPCOUNT indentifier
+const unsigned int c_size = MAX_SIZE;
+
+// Computes matrix multiply
+// C = AxB, where A, B and C are square matrices of dimension (sizexsize)
+extern "C" {
+void mmult(const int *in1, // Read-Only Matrix 1
+           const int *in2, // Read-Only Matrix 2
+           int *out_r,     // Output Result
+           int size        // Size of one dimension of the matrices
+) {
+   #pragma HLS INTERFACE m_axi port=in1 offset=slave bundle=gmem
+   #pragma HLS INTERFACE m_axi port=in2 offset=slave bundle=gmem
+   #pragma HLS INTERFACE m_axi port=out_r offset=slave bundle=gmem
+
+   #pragma HLS INTERFACE s_axilite port=in1 bundle=control
+   #pragma HLS INTERFACE s_axilite port=in2 bundle=control
+   #pragma HLS INTERFACE s_axilite port=out_r bundle=control
+   #pragma HLS INTERFACE s_axilite port=size bundle=control
+   #pragma HLS INTERFACE s_axilite port=return bundle=control
+
+    // Local memory to store input and output matrices
+    // Local memory is implemented as BRAM memory blocks
+    int A[MAX_SIZE][MAX_SIZE];
+    int B[MAX_SIZE][MAX_SIZE];
+    int C[MAX_SIZE][MAX_SIZE];
+    int temp_sum[MAX_SIZE];
+    #pragma HLS ARRAY_PARTITION variable = B dim = 2 complete
+    #pragma HLS ARRAY_PARTITION variable = C dim = 2 complete
+    #pragma HLS ARRAY_PARTITION variable = temp_sum dim = 1 complete
+
+// Burst reads on input matrices from global memory
+// Burst read for matrix A
+readA:
+    for (int itr = 0, i = 0, j = 0; itr < size * size; itr++, j++) {
+       #pragma HLS PIPELINE II=1
+       #pragma HLS LOOP_TRIPCOUNT min=c_size*c_size max=c_size*c_size
+        if (j == size) {
+            j = 0;
+            i++;
+        }
+        A[i][j] = in1[itr];
+    }
+
+// Burst read for matrix B
+readB:
+    for (int itr = 0, i = 0, j = 0; itr < size * size; itr++, j++) {
+       #pragma HLS PIPELINE II=1
+       #pragma HLS LOOP_TRIPCOUNT min=c_size*c_size max=c_size*c_size
+        if (j == size) {
+            j = 0;
+            i++;
+        }
+        B[i][j] = in2[itr];
+    }
+
+mmult1:
+    for (int i = 0; i < size; i++) {
+       #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+    mmult2:
+        for (int k = 0; k < size; k++) {
+           #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+           #pragma HLS PIPELINE II=1
+        mmult3:
+            for (int j = 0; j < MAX_SIZE; j++) {
+                int result = (k == 0) ? 0 : temp_sum[j];
+                result += A[i][k] * B[k][j];
+                temp_sum[j] = result;
+                if (k == size - 1)
+                    C[i][j] = result;
+            }
+        }
+    }
+
+// Burst write from output matrices to global memory
+// Burst write from matrix C
+writeC:
+    for (int itr = 0, i = 0, j = 0; itr < size * size; itr++, j++) {
+       #pragma HLS PIPELINE II=1
+       #pragma HLS LOOP_TRIPCOUNT min=c_size*c_size max=c_size*c_size
+        if (j == size) {
+            j = 0;
+            i++;
+        }
+        out_r[itr] = C[i][j];
+    }
+}
+}
