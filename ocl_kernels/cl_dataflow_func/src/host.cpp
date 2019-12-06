caabbd96753511fe27f4aa6a828c4e15c09de9c2
Adarsh Kapil <adarsh.kapil@xilinx.com>
2019-12-06 15:24:18 +0530

    multiple device support for ocl kernels

--- ocl_kernels/cl_dataflow_func/src/host.cpp
+++ ocl_kernels/cl_dataflow_func/src/host.cpp
@@ -76,8 +76,7 @@ int main(int argc, char **argv) {
 
         std::cout << "Trying to program device[" << i
                   << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
-        OCL_CHECK(err,
-                  cl::Program program(context, {device}, bins, NULL, &err));
+        cl::Program program(context, {device}, bins, NULL, &err);
         if (err != CL_SUCCESS) {
             std::cout << "Failed to program device[" << i
                       << "] with xclbin file!\n";
