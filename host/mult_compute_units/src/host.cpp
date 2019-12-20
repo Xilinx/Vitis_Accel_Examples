7e1078be06b73258f315bdc44bb7af7608c00128
Adarsh Kapil <adarsh.kapil@xilinx.com>
2019-12-20 15:48:48 +0530

    licence changes

--- host/mult_compute_units/src/host.cpp
+++ host/mult_compute_units/src/host.cpp
@@ -1,16 +1,21 @@
 /**********
 Copyright (c) 2019, Xilinx, Inc.
 All rights reserved.
+
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
+
 1. Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
+
 2. Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
+
 3. Neither the name of the copyright holder nor the names of its contributors
 may be used to endorse or promote products derived from this software
 without specific prior written permission.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -20,11 +25,11 @@ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR B
 HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********/
 
-This is simple Example of Multiple Compute units to showcase how a single kernel
+/*This is simple Example of Multiple Compute units to showcase how a single kernel
 can be instantiated into Multiple compute units. Host code will show how to use 
-multiple compute units and run them concurrently.
-*************************************************/
+multiple compute units and run them concurrently. */
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
--- rtl_kernels/rtl_streaming_k2k_mm/src/krnl_s2mm.cpp
+++ rtl_kernels/rtl_streaming_k2k_mm/src/krnl_s2mm.cpp
@@ -1,5 +1,4 @@
 /**********
-/**********
 Copyright (c) 2019, Xilinx, Inc.
 All rights reserved.
 
