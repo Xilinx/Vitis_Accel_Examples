4f7adfc7d0cdc8ec95ea906cf0236dadc8dea442
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2020-05-14 18:15:43 +0530

    Added new simple vadd design to track tool version (#367)

---
+++ cpp_kernels/simple_vadd/src/krnl_vadd.cpp
@@ -0,0 +1,90 @@
+/**********
+Copyright (c) 2019, Xilinx, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********/
+
+//------------------------------------------------------------------------------
+//
+// kernel:  vadd
+//
+// Purpose: Demonstrate Vector Add Kernel
+//
+
+#define BUFFER_SIZE 256
+#define DATA_SIZE 4096 
+//TRIPCOUNT identifier
+const unsigned int c_len = DATA_SIZE / BUFFER_SIZE;
+const unsigned int c_size = BUFFER_SIZE;
+
+/*
+    Vector Addition Kernel Implementation 
+    Arguments:
+        in1   (input)     --> Input Vector1
+        in2   (input)     --> Input Vector2
+        out_r   (output)    --> Output Vector
+        size  (input)     --> Size of Vector in Integer
+*/
+
+extern "C" {
+void krnl_vadd(const unsigned int *in1, // Read-Only Vector 1
+          const unsigned int *in2, // Read-Only Vector 2
+          unsigned int *out_r,     // Output Result
+          int size                 // Size in integer
+) {
+
+    unsigned int v1_buffer[BUFFER_SIZE];   // Local memory to store vector1
+
+    //Per iteration of this loop perform BUFFER_SIZE vector addition
+    for (int i = 0; i < size; i += BUFFER_SIZE) {
+       #pragma HLS LOOP_TRIPCOUNT min=c_len max=c_len
+        int chunk_size = BUFFER_SIZE;
+        //boundary checks
+        if ((i + BUFFER_SIZE) > size)
+            chunk_size = size - i;
+
+        read1: for (int j = 0; j < chunk_size; j++) {
+           #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+            v1_buffer[j] = in1[i + j];
+        }
+
+        //Burst reading B and calculating C and Burst writing 
+        // to  Global memory
+        vadd_writeC: for (int j = 0; j < chunk_size; j++) {
+           #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+            //perform vector addition
+            out_r[i+j] = v1_buffer[j] + in2[i+j];
+        }
+
+    }
+}
+}
