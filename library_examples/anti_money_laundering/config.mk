4b5abfd5bb7644f671ee2393a4c04c485092a28e
Virat Agarwal <virat.agarwal@xilinx.com>
2020-09-08 09:25:30 +0530

    Adding config.mk for gcc settings for library_examples/anti_money_laundering (#521)

---
+++ library_examples/anti_money_laundering/config.mk
@@ -0,0 +1,28 @@
+#
+# Copyright 2020 Xilinx, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#Checks for g++
+CXX := g++
+ifeq ($(HOST_ARCH), x86)
+ifneq ($(shell expr $(shell g++ -dumpversion) \>= 5), 1)
+ifndef XILINX_VIVADO
+$(error [ERROR]: g++ version older. Please use 5.0 or above)
+else
+CXX := $(XILINX_VIVADO)/tps/lnx64/gcc-6.2.0/bin/g++
+$(warning [WARNING]: g++ version older. Using g++ provided by the tool : $(CXX))
+endif
+endif
+endif
