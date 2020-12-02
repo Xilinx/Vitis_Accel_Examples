18f21f21aad23d0ebb3937213c4a0e9da8ac901e
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2020-12-02 09:45:36 +0530

    update validate designs makefile to support user ini file

--- validate/verify_test/config.mk
+++ validate/verify_test/config.mk
@@ -1,7 +1,16 @@
 GEN_DIR = ./test
+USR_CLFLAGS:=
+USR_LDCLFLAGS:=
 
 platform_test:
 	platforminfo -j $(DEVICE) > platform_info.json
 	$(ABS_COMMON_REPO)/common/utility/platform_gen.py platform_info.json verify
+
+ifneq ($(USR_CLFLAGS), )	
+CLFLAGS += $(USR_CLFLAGS)
+endif
+ifneq ($(USR_LDCLFLAGS), )
+LDCLFLAGS += $(USR_LDCLFLAGS)
+endif
 
 PLATFORM_JSON=platform.json
