6ec61011fc2def55bfbb66a0e0bd73fcacdbc330
Virat Agarwal <virat.agarwal@xilinx.com>
2021-03-17 22:10:09 +0530

    Updating cpp_kernels/simple_vadd example with OCL_CHECKs

--- cpp_kernels/simple_vadd/src/vadd.cpp
+++ cpp_kernels/simple_vadd/src/vadd.cpp
@@ -13,6 +13,14 @@
 * License for the specific language governing permissions and limitations
 * under the License.
 */
+
+#define OCL_CHECK(error, call)                                                                   \
+    call;                                                                                        \
+    if (error != CL_SUCCESS) {                                                                   \
+        printf("%s:%d Error calling " #call ", error code is: %d\n", __FILE__, __LINE__, error); \
+        exit(EXIT_FAILURE);                                                                      \
+    }
+
 #include "vadd.h"
 #include <fstream>
 #include <iostream>
@@ -31,7 +39,7 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    char* xclbinFilename = argv[1];
+    std::string xclbinFilename = argv[1];
 
     // Compute the size of array in bytes
     size_t size_in_bytes = DATA_SIZE * sizeof(int);
@@ -41,6 +49,11 @@ int main(int argc, char* argv[]) {
 
     std::vector<cl::Device> devices;
     cl::Device device;
+    cl_int err;
+    cl::Context context;
+    cl::CommandQueue q;
+    cl::Kernel krnl_vector_add;
+    cl::Program program;
     std::vector<cl::Platform> platforms;
     bool found_device = false;
 
@@ -66,9 +79,15 @@ int main(int argc, char* argv[]) {
     }
 
     // Creating Context and Command Queue for selected device
-    cl::Context context(device);
-    cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+    OCL_CHECK(err, context = cl::Context(device, NULL, NULL, NULL, &err));
+    OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
 
+    std::cout << "INFO: Reading " << xclbinFilename << std::endl;
+    FILE* fp;
+    if ((fp = fopen(xclbinFilename.c_str(), "r")) == nullptr) {
+        printf("ERROR: %s xclbin not available please build\n", xclbinFilename.c_str());
+        exit(EXIT_FAILURE);
+    }
     // Load xclbin
     std::cout << "Loading: '" << xclbinFilename << "'\n";
     std::ifstream bin_file(xclbinFilename, std::ifstream::binary);
@@ -82,49 +101,48 @@ int main(int argc, char* argv[]) {
     cl::Program::Binaries bins;
     bins.push_back({buf, nb});
     devices.resize(1);
-    cl::Program program(context, devices, bins);
+    OCL_CHECK(err, program = cl::Program(context, devices, bins, NULL, &err));
 
     // This call will get the kernel object from program. A kernel is an
     // OpenCL function that is executed on the FPGA.
-    cl::Kernel krnl_vector_add(program, "krnl_vadd");
+    OCL_CHECK(err, krnl_vector_add = cl::Kernel(program, "krnl_vadd", &err));
 
-    // These commands will allocate memory on the Device. The cl::Buffer objects
-    // can
+    // These commands will allocate memory on the Device. The cl::Buffer objects can
     // be used to reference the memory locations on the device.
-    cl::Buffer buffer_a(context, CL_MEM_READ_ONLY, size_in_bytes);
-    cl::Buffer buffer_b(context, CL_MEM_READ_ONLY, size_in_bytes);
-    cl::Buffer buffer_result(context, CL_MEM_WRITE_ONLY, size_in_bytes);
+    OCL_CHECK(err, cl::Buffer buffer_a(context, CL_MEM_READ_ONLY, size_in_bytes, NULL, &err));
+    OCL_CHECK(err, cl::Buffer buffer_b(context, CL_MEM_READ_ONLY, size_in_bytes, NULL, &err));
+    OCL_CHECK(err, cl::Buffer buffer_result(context, CL_MEM_WRITE_ONLY, size_in_bytes, NULL, &err));
 
     // set the kernel Arguments
     int narg = 0;
-    krnl_vector_add.setArg(narg++, buffer_a);
-    krnl_vector_add.setArg(narg++, buffer_b);
-    krnl_vector_add.setArg(narg++, buffer_result);
-    krnl_vector_add.setArg(narg++, DATA_SIZE);
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, buffer_a));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, buffer_b));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, buffer_result));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++, DATA_SIZE));
 
     // We then need to map our OpenCL buffers to get the pointers
-    int* ptr_a = (int*)q.enqueueMapBuffer(buffer_a, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes);
-    int* ptr_b = (int*)q.enqueueMapBuffer(buffer_b, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes);
-    int* ptr_result = (int*)q.enqueueMapBuffer(buffer_result, CL_TRUE, CL_MAP_READ, 0, size_in_bytes);
-
-    // setting input data
-    for (int i = 0; i < DATA_SIZE; i++) {
-        ptr_a[i] = 10;
-        ptr_b[i] = 20;
-    }
+    int* ptr_a;
+    int* ptr_b;
+    int* ptr_result;
+    OCL_CHECK(err,
+              ptr_a = (int*)q.enqueueMapBuffer(buffer_a, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes, NULL, NULL, &err));
+    OCL_CHECK(err,
+              ptr_b = (int*)q.enqueueMapBuffer(buffer_b, CL_TRUE, CL_MAP_WRITE, 0, size_in_bytes, NULL, NULL, &err));
+    OCL_CHECK(err, ptr_result = (int*)q.enqueueMapBuffer(buffer_result, CL_TRUE, CL_MAP_READ, 0, size_in_bytes, NULL,
+                                                         NULL, &err));
 
     // Data will be migrated to kernel space
-    q.enqueueMigrateMemObjects({buffer_a, buffer_b}, 0 /* 0 means from host*/);
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_a, buffer_b}, 0 /* 0 means from host*/));
 
     // Launch the Kernel
-    q.enqueueTask(krnl_vector_add);
+    OCL_CHECK(err, err = q.enqueueTask(krnl_vector_add));
 
     // The result of the previous kernel execution will need to be retrieved in
     // order to view the results. This call will transfer the data from FPGA to
     // source_results vector
-    q.enqueueMigrateMemObjects({buffer_result}, CL_MIGRATE_MEM_OBJECT_HOST);
+    OCL_CHECK(err, q.enqueueMigrateMemObjects({buffer_result}, CL_MIGRATE_MEM_OBJECT_HOST));
 
-    q.finish();
+    OCL_CHECK(err, q.finish());
 
     // Verify the result
     int match = 0;
@@ -137,10 +155,10 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    q.enqueueUnmapMemObject(buffer_a, ptr_a);
-    q.enqueueUnmapMemObject(buffer_b, ptr_b);
-    q.enqueueUnmapMemObject(buffer_result, ptr_result);
-    q.finish();
+    OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_a, ptr_a));
+    OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_b, ptr_b));
+    OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_result, ptr_result));
+    OCL_CHECK(err, err = q.finish());
 
     std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
     return (match ? EXIT_FAILURE : EXIT_SUCCESS);
