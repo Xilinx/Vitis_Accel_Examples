e0d33b6fb6cea2d1bd489db668b1fcb75f7ede24
Virat Agarwal <virat.agarwal@xilinx.com>
2021-04-08 14:34:48 +0530

    Making 2021.1_ specific changes for external_io in the next branch

--- emulation/external_io/master.sh
+++ emulation/external_io/master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "master launched"
 echo "$PWD"
-export PYTHONPATH=$XILINX_VIVADO/data/emulation/hw_em/lib/python:$XILINX_VIVADO/data/python/xtlm_ipc/xtlm_ipc_v1_0
+export PYTHONPATH=$XILINX_VIVADO/data/emulation/hw_em/lib/python:$XILINX_VIVADO/data/emulation/ip_utils/xtlm_ipc/xtlm_ipc_v1_0/python
 echo " py path is $PYTHONPATH "
 export LD_LIBRARY_PATH=$XILINX_VIVADO/tps/lnx64/python-3.8.3/lib/:$LD_LIBRARY_PATH
 $XILINX_VIVADO/tps/lnx64/python-3.8.3/bin/python3 ./test_master.py
--- emulation/external_io/py_exec.sh
+++ emulation/external_io/py_exec.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #stream_scripts
-sleep 200
 echo "master launched"
 (./master.sh >  master.log ) &
 echo "slave launched" 
