20a148026f02de65aef9de960a21ad243b890831
Virat Agarwal <virat.agarwal@xilinx.com>
2021-04-12 17:47:50 +0530

    Updating vadd kernel for hello world to use inter module streams

--- hello_world/src/vadd.cpp
+++ hello_world/src/vadd.cpp
@@ -16,93 +16,114 @@
 
 /*******************************************************************************
 Description:
-    HLS pragmas can be used to optimize the design : improve throughput, reduce
-latency and
-    device resource utilization of the resulting RTL code
-    This is vector addition example to demonstrate how HLS optimizations are
-used in kernel.
+    This is example of vector addition to demonstrate HLS Dataflow Pragma
+    functionality to perform task level parallelism using HLS Stream datatype.
+HLS
+    dataflow pragma instruct compiler to run sub-task parallel. Sub-task can
+    transfer the data using hls stream. In this Example, a vector addition
+    implementation is divided into three sub-task APIs as below:
+    1) read_input():
+        This API reads the input vector from Global Memory and writes it into
+        HLS Stream 'inStream' using blocking write command.
+    2) compute_add():
+        This API reads the input vector from 'inStream' using blocking read
+        command and increment the value by user specified increment. It writes
+        the results into 'outStream' using blocking write command.
+    3) write_result():
+        This API reads the result vector from 'outStream' using blocking read
+        command and write the result into Global Memory Location.
+    Data Flow Stream based Adder will be implemented as below:
+                    _____________
+                    |             |<----- Input Vectors from Global Memory
+                    |  read_input |       __
+                    |_____________|----->|  |
+                     _____________       |  | inStream
+                    |             |<-----|__|
+                    | compute_add |       __
+                    |_____________|----->|  |
+                     _____________       |  | outStream
+                    |              |<----|__|
+                    | write_result |
+                    |______________|-----> Output result to Global Memory
 *******************************************************************************/
 
-#define BUFFER_SIZE 1024
+// Includes
+#include <ap_int.h>
+#include <hls_stream.h>
+
 #define DATA_SIZE 4096
 
 // TRIPCOUNT identifier
-const unsigned int c_len = DATA_SIZE / BUFFER_SIZE;
-const unsigned int c_size = BUFFER_SIZE;
+const int c_size = DATA_SIZE;
 
-/*
-    Vector Addition Kernel Implementation
-    Arguments:
-        in1   (input)     --> Input Vector1
-        in2   (input)     --> Input Vector2
-        out_r   (output)    --> Output Vector
-        size  (input)     --> Size of Vector in Integer
-*/
+// Read Data from Global Memory and write into Stream inStream
+static void read_input(unsigned int* in, hls::stream<unsigned int>& inStream, int size) {
+// Auto-pipeline is going to apply pipeline to this loop
+mem_rd:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking write command to inStream
+        inStream << in[i];
+    }
+}
+
+// Read Input data from inStream and write the result into outStream
+static void compute_add(hls::stream<unsigned int>& inStream1,
+                        hls::stream<unsigned int>& inStream2,
+                        hls::stream<unsigned int>& outStream,
+                        int size) {
+// Auto-pipeline is going to apply pipeline to this loop
+execute:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking read command from inStream and Blocking write command
+        // to outStream
+        outStream << (inStream1.read() + inStream2.read());
+    }
+}
+
+// Read result from outStream and write the result to Global Memory
+static void write_result(unsigned int* out, hls::stream<unsigned int>& outStream, int size) {
+// Auto-pipeline is going to apply pipeline to this loop
+mem_wr:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        // Blocking read command to inStream
+        out[i] = outStream.read();
+    }
+}
 
 extern "C" {
-void vadd(const unsigned int* in1, // Read-Only Vector 1
-          const unsigned int* in2, // Read-Only Vector 2
-          unsigned int* out_r,     // Output Result
-          int size                 // Size in integer
-          ) {
-    // In Vitis flow, a kernel can be defined without interface pragma. For such
-    // case, it follows default behavioral.
-    // All pointer arguments (in1,in2,out_r) will be referred as Memory Mapped
-    // pointers using single M_AXI interface.
-    // All the scalar arguments (size) and return argument will be associated to
-    // single s_axilite interface.
-    unsigned int v1_buffer[BUFFER_SIZE];   // Local memory to store vector1
-    unsigned int v2_buffer[BUFFER_SIZE];   // Local memory to store vector2
-    unsigned int vout_buffer[BUFFER_SIZE]; // Local Memory to store result
+/*
+    Vector Addition Kernel Implementation using dataflow
+    Arguments:
+        in1   (input)  --> Input Vector 1
+        in2   (input)  --> Input Vector 2
+        out  (output) --> Output Vector
+        size (input)  --> Size of Vector in Integer
+   */
+void vadd(unsigned int* in1, unsigned int* in2, unsigned int* out, int size) {
+    // Adding names for the streams. It allows the name to be used in reporting.
+    // Vivado HLS
+    // automatically checks to ensure all elements from an input stream are read.
+    static hls::stream<unsigned int> inStream1("input_stream_1");
+    static hls::stream<unsigned int> inStream2("input_stream_2");
+    static hls::stream<unsigned int> outStream("output_stream");
+#pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
+#pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
+#pragma HLS INTERFACE m_axi port = out bundle = gmem0
+//  If Stream is empty (no element in
+//  Stream)
+//  any blocking read command from consumer will go into wait state until
+//  producer
+//  writes elements to Stream. This blocking read and write allow consumer and
+//  producer to synchronize each other.
 
-    // Per iteration of this loop perform BUFFER_SIZE vector addition
-    for (int i = 0; i < size; i += BUFFER_SIZE) {
-#pragma HLS LOOP_TRIPCOUNT min = c_len max = c_len
-        int chunk_size = BUFFER_SIZE;
-        // boundary checks
-        if ((i + BUFFER_SIZE) > size) chunk_size = size - i;
-
-    // Transferring data in bursts hides the memory access latency as well as
-    // improves bandwidth utilization and efficiency of the memory controller.
-    // It is recommended to infer burst transfers from successive requests of data
-    // from consecutive address locations.
-    // A local memory vl_local is used for buffering the data from a single burst.
-    // The entire input vector is read in multiple bursts.
-    // The choice of LOCAL_MEM_SIZE depends on the specific applications and
-    // available on-chip memory on target FPGA.
-    // burst read of v1 and v2 vector from global memory
-
-    read1:
-        for (int j = 0; j < chunk_size; j++) {
-#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-#pragma HLS PIPELINE II = 1
-            v1_buffer[j] = in1[i + j];
-        }
-
-    read2:
-        for (int j = 0; j < chunk_size; j++) {
-#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-#pragma HLS PIPELINE II = 1
-            v2_buffer[j] = in2[i + j];
-        }
-
-    // PIPELINE pragma reduces the initiation interval for loop by allowing the
-    // concurrent executions of operations
-    vadd:
-        for (int j = 0; j < chunk_size; j++) {
-#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-#pragma HLS PIPELINE II = 1
-            // perform vector addition
-            vout_buffer[j] = v1_buffer[j] + v2_buffer[j];
-        }
-
-    // burst write the result
-    write:
-        for (int j = 0; j < chunk_size; j++) {
-#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
-#pragma HLS PIPELINE II = 1
-            out_r[i + j] = vout_buffer[j];
-        }
-    }
+#pragma HLS dataflow
+    // dataflow pragma instruct compiler to run following three APIs in parallel
+    read_input(in1, inStream1, size);
+    read_input(in2, inStream2, size);
+    compute_add(inStream1, inStream2, outStream, size);
+    write_result(out, outStream, size);
 }
 }
