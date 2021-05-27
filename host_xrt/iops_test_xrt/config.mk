e94d12d214b3a31800db4a0d0e14730a6e693e3c
Bhaskar VishnuVardhan Chebrolu <bchebrol@xilinx.com>
2021-05-27 17:54:02 +0530

    update native xrt designs to support different OS's and versions

--- host_xrt/iops_test_xrt/config.mk
+++ host_xrt/iops_test_xrt/config.mk
@@ -1,3 +1,20 @@
+OSDIST := $(shell lsb_release -i )
+OSREL := $(shell lsb_release -r )
+
 ifeq ($(HOST_ARCH), x86)
+ifeq ($(findstring CentOS, $(OSDIST)), CentOS)
+ifeq ($(findstring 7., $(OSREL)), 7.)
 CXXFLAGS += -D_GLIBCXX_USE_CXX11_ABI=0
+endif
+endif
+ifeq ($(findstring RedHat, $(OSDIST)), RedHat)
+ifeq ($(findstring 7., $(OSREL)), 7.)
+CXXFLAGS += -D_GLIBCXX_USE_CXX11_ABI=0
+endif
+endif
+ifeq ($(findstring Ubuntu, $(OSDIST)), Ubuntu)
+ifeq ($(findstring 16., $(OSREL)), 16.)
+CXXFLAGS += -D_GLIBCXX_USE_CXX11_ABI=0
+endif
+endif
 endif
--- host_xrt/p2p_simple_xrt/config.mk
+++ host_xrt/p2p_simple_xrt/config.mk
@@ -1,6 +1,24 @@
+OSDIST := $(shell lsb_release -i )
+OSREL := $(shell lsb_release -r )
+
 ifeq ($(HOST_ARCH), x86)
+ifeq ($(findstring CentOS, $(OSDIST)), CentOS)
+ifeq ($(findstring 7., $(OSREL)), 7.)
 CXXFLAGS += -D_GLIBCXX_USE_CXX11_ABI=0
 endif
+endif
+ifeq ($(findstring RedHat, $(OSDIST)), RedHat)
+ifeq ($(findstring 7., $(OSREL)), 7.)
+CXXFLAGS += -D_GLIBCXX_USE_CXX11_ABI=0
+endif
+endif
+ifeq ($(findstring Ubuntu, $(OSDIST)), Ubuntu)
+ifeq ($(findstring 16., $(OSREL)), 16.)
+CXXFLAGS += -D_GLIBCXX_USE_CXX11_ABI=0
+endif
+endif
+endif
+
 DRIVE_PATH:=
 ifneq ($(DRIVE_PATH), )
 	CMD_ARGS += -p $(DRIVE_PATH)
