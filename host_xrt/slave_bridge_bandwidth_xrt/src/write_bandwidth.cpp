0b559e30e09ef70a0d1a0b7846e0419dc0d15f37
Virat Agarwal <virat.agarwal@xilinx.com>
2021-06-03 22:49:34 +0530

    Adding qor next

--- host_xrt/slave_bridge_bandwidth_xrt/src/write_bandwidth.cpp
+++ host_xrt/slave_bridge_bandwidth_xrt/src/write_bandwidth.cpp
@@ -39,10 +39,10 @@ void write_bandwidth(TYPE* output0, int64_t buf_size, int64_t iter) {
     uint32_t baseAddr = 0;
 
     if (buf_size <= 8 * 1024) {
+    write_bandwidth_small_1:
         for (int itr = 0; itr < iter * factor; itr++) {
-#pragma HLS PIPELINE II = 1
+        write_bandwidth_small_2:
             for (int i = 0; i < c_burstLength; i++) {
-#pragma HLS PIPELINE II = 1
                 output0[baseAddr + i] = 1;
             }
             Indx = itr % factor;
@@ -50,7 +50,9 @@ void write_bandwidth(TYPE* output0, int64_t buf_size, int64_t iter) {
         }
     } else {
         buf_size = buf_size / c_widthInBytes;
+    write_bandwidth_large_1:
         for (int64_t i = 0; i < iter; i++) {
+        write_bandwidth_large_2:
             for (int64_t blockindex = 0; blockindex < buf_size; blockindex++) {
                 output0[blockindex] = 1;
             }
