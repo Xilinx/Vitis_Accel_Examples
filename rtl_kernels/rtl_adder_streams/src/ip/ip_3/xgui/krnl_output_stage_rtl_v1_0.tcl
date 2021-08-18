438d632534b673205bc53417a327023c4bb37d01
Virat Agarwal <virat.agarwal@xilinx.com>
2021-08-18 09:33:41 +0530

    Adding component.xml updated

--- rtl_kernels/rtl_adder_streams/src/ip/ip_3/xgui/krnl_output_stage_rtl_v1_0.tcl
+++ rtl_kernels/rtl_adder_streams/src/ip/ip_3/xgui/krnl_output_stage_rtl_v1_0.tcl
@@ -3,8 +3,14 @@ proc init_gui { IPINST } {
   #Adding Page
   ipgui::add_page $IPINST -name "Page 0"
 
+  #Adding Page
+  set _Clocks_ [ipgui::add_page $IPINST -name "_Clocks_"]
+  ipgui::add_static_text $IPINST -name "clock_text" -parent ${_Clocks_} -text {Enter the target frequency for the input clock(s) for the IP.
+These frequencies will be used during the default out-of-context synthesis flow}
+
 
 }
+
 
 proc update_MODELPARAM_VALUE.C_S_AXI_CONTROL_DATA_WIDTH { MODELPARAM_VALUE.C_S_AXI_CONTROL_DATA_WIDTH } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
