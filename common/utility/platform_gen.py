d121be3a639fdd989fea49476514ba84fce08b2c
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2021-08-18 15:03:16 +0530

    fix platform_gen python script

--- common/utility/platform_gen.py
+++ common/utility/platform_gen.py
@@ -142,11 +142,11 @@ def create_json(data):
                 if "raptor2" in data["hardwarePlatforms"]["hardwarePlatform"]["extensions"]:
                     if "feature_support" in data["hardwarePlatforms"]["hardwarePlatform"]["extensions"]["raptor2"]:
                         feature_support = data["hardwarePlatforms"]["hardwarePlatform"]["extensions"]["raptor2"]["feature_support"]
-                        if "DDR" in feature_support and (filename!='slavebridge'):
-                       	    Memory_list[0] = int(feature_support["DDR"])
-                        if "HOST" in feature_support and (filename=='slavebridge'):
+                        if "DDR" in feature_support:
+                            Memory_list[0] = int(feature_support["DDR"])
+                        if "HOST" in feature_support:
                             Memory_list[1] = int(feature_support["HOST"])
-                        if "HBM" in feature_support and (filename!='slavebridge'):
+                        if "HBM" in feature_support:
                             Memory_list[2] = int(feature_support["HBM"])
     
     Total_ddr_banks = Memory_list[0]
