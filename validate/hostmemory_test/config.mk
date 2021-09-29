2884241ad57879eb54d5be1bad64ff81c00f1ef1
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2021-09-29 11:33:23 +0530

    validate designs boost_system-mt flag insert

--- validate/hostmemory_test/config.mk
+++ validate/hostmemory_test/config.mk
@@ -2,6 +2,22 @@ GEN_DIR = ./test
 USR_VPP_FLAGS:=
 USR_VPP_LDFLAGS:=
 
+OSDIST := $(shell lsb_release -i )	
+OSREL := $(shell lsb_release -r )	
+
+ifeq ($(HOST_ARCH), x86)	
+ifeq ($(findstring CentOS, $(OSDIST)), CentOS)	
+ifeq ($(findstring 7., $(OSREL)), 7.)	
+LDFLAGS += -lboost_system-mt
+endif	
+endif	
+ifeq ($(findstring RedHat, $(OSDIST)), RedHat)	
+ifeq ($(findstring 7., $(OSREL)), 7.)	
+LDFLAGS += -lboost_system-mt	
+endif	
+endif
+endif
+
 platform_test:
 	platforminfo -j -d $(DEVICE) > platform_info.json
 	$(XF_PROJ_ROOT)/common/utility/platform_gen.py platform_info.json hostmemory
