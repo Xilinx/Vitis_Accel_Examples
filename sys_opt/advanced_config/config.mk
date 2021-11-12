ec87464a3d0b5185e194634ace4afb329074722f
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2021-11-12 17:13:33 +0530

    provided support for vck5000 platform

--- sys_opt/advanced_config/config.mk
+++ sys_opt/advanced_config/config.mk
@@ -1,7 +1,7 @@
 ifeq ($(findstring zc, $(PLATFORM)), zc)
 VPP_LDFLAGS += --config vadd_soc.cfg
 VPP_FLAGS += --config vadd_vadd_soc.cfg
-else ifeq ($(findstring vck, $(PLATFORM)), vck)
+else ifeq ($(findstring vck190, $(PLATFORM)), vck190)
 VPP_LDFLAGS += --config vadd_soc.cfg
 VPP_FLAGS += --config vadd_vadd_soc.cfg
 else
