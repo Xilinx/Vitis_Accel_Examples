0a7888124b703e8fa9ebd06b3979cf9b72e93381
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2022-01-05 16:53:58 +0530

    updated host_global_bw with multiple platforms support

--- performance/host_global_bandwidth/src/host.cpp
+++ performance/host_global_bandwidth/src/host.cpp
@@ -181,14 +181,9 @@ int main(int argc, char** argv) {
         int nxtcnt = buff_tab[buff_size_1][0];
         int buff_cnt = buff_tab[buff_size_1][1];
         std::vector<cl::Memory> mems(buff_cnt);
-        cl_mem_ext_ptr_t bufExt[buff_cnt];
 
         for (int i = buff_cnt - 1; i >= 0; i--) {
-            bufExt[i].flags = XCL_MEM_DDR_BANK1;
-            bufExt[i].obj = nullptr;
-            bufExt[i].param = 0;
-            OCL_CHECK(err, mems[i] = cl::Buffer(context, (cl_mem_flags)(CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX),
-                                                nxtcnt, &bufExt[i], &err));
+            OCL_CHECK(err, mems[i] = cl::Buffer(context, (cl_mem_flags)(CL_MEM_READ_WRITE), nxtcnt, nullptr, &err));
             OCL_CHECK(err, err = command_queue.enqueueFillBuffer<int>((cl::Buffer&)mems[i], i, 0, nxtcnt, 0, 0));
         }
 
@@ -215,23 +210,11 @@ int main(int argc, char** argv) {
         int buff_cnt = buff_tab[buff_size_1][1];
         std::vector<cl::Memory> mems1(buff_cnt);
         std::vector<cl::Memory> mems2(buff_cnt);
-        cl_mem_ext_ptr_t bufExt1[buff_cnt];
-        cl_mem_ext_ptr_t bufExt2[buff_cnt];
 
         for (int i = buff_cnt - 1; i >= 0; i--) {
-            bufExt1[i].flags = XCL_MEM_DDR_BANK0;
-            bufExt1[i].obj = nullptr;
-            bufExt1[i].param = 0;
-
-            bufExt2[i].flags = XCL_MEM_DDR_BANK1;
-            bufExt2[i].obj = nullptr;
-            bufExt2[i].param = 0;
-
-            OCL_CHECK(err, mems1[i] = cl::Buffer(context, (cl_mem_flags)(CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX),
-                                                 nxtcnt, &bufExt1[i], &err));
+            OCL_CHECK(err, mems1[i] = cl::Buffer(context, (cl_mem_flags)(CL_MEM_READ_WRITE), nxtcnt, nullptr, &err));
             OCL_CHECK(err, err = command_queue.enqueueFillBuffer<int>((cl::Buffer&)mems1[i], i, 0, nxtcnt, 0, 0));
-            OCL_CHECK(err, mems2[i] = cl::Buffer(context, (cl_mem_flags)(CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX),
-                                                 nxtcnt, &bufExt2[i], &err));
+            OCL_CHECK(err, mems2[i] = cl::Buffer(context, (cl_mem_flags)(CL_MEM_READ_WRITE), nxtcnt, nullptr, &err));
             OCL_CHECK(err, err = command_queue.enqueueFillBuffer<int>((cl::Buffer&)mems2[i], i, 0, nxtcnt, 0, 0));
         }
 
