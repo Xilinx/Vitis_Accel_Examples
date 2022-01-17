33a7a8276982d29b05b994fbf98c8d9910b97290
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2022-01-17 16:25:50 +0530

    rename device to platform in opencl.mk

--- common/includes/opencl/opencl.mk
+++ common/includes/opencl/opencl.mk
@@ -12,12 +12,12 @@ opencl_CXXFLAGS=-I$(OPENCL_INCLUDE) -I$(VIVADO_INCLUDE)
 OPENCL_LIB:= $(xrt_path)/lib
 opencl_LDFLAGS=-L$(OPENCL_LIB) -lOpenCL -pthread
 
-ifeq ($(findstring vck, $(DEVICE)), vck)		
+ifeq ($(findstring vck, $(PLATFORM)), vck)		
 	VPP_PFLAGS+=--package.sd_dir /proj/xbuilds/2022.1_daily_latest/internal_platforms/sw/versal/xrt		
 endif		
-ifeq ($(findstring zcu, $(DEVICE)), zcu)		
+ifeq ($(findstring zcu, $(PLATFORM)), zcu)		
 	VPP_PFLAGS+=--package.sd_dir /proj/xbuilds/2022.1_daily_latest/internal_platforms/sw/zynqmp/xrt		
 endif		
-ifeq ($(findstring zc7, $(DEVICE)), zc7)		
+ifeq ($(findstring zc7, $(PLATFORM)), zc7)		
 	VPP_PFLAGS+=--package.sd_dir /proj/xbuilds/2022.1_daily_latest/internal_platforms/sw/zynq/xrt		
 endif
