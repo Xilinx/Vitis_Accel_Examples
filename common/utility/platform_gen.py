54cc77351887416b8d74d8ca08eb57e6a56eeda9
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2022-02-02 14:11:45 +0530

    support cs2211 versal platform for validate designs

--- common/utility/platform_gen.py
+++ common/utility/platform_gen.py
@@ -257,7 +257,7 @@ if "_u2_" in plat_name:
     if(filename!='verify'):
         create_cfg_u2(data)
     create_json_u2(data)
-elif "v65" in plat_name or "vck5000" in plat_name or "cs2200" in plat_name:
+elif "v65" in plat_name or "vck5000" in plat_name or "cs2200" in plat_name or "cs2211" in plat_name:
     if(filename!='verify'):
         create_cfg_vck(data)
     create_json_vck(data)
