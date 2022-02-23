8892bcf550e4fba1922235baea5f404082f97a93
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2022-02-23 16:12:37 +0530

    updated the hello world python design by cleaning ctypes and numpy

--- host_py/hello_world_py/src/host.py
+++ host_py/hello_world_py/src/host.py
@@ -16,10 +16,10 @@
 
 import os
 import sys
-import numpy
+import uuid
+import re
 
 # Following found in PYTHONPATH setup by XRT
-from xrt_binding import *
 import pyxrt
 
 from utils_binding import *
@@ -29,38 +29,44 @@ def runKernel(opt):
     xbin = pyxrt.xclbin(opt.bitstreamFile)
     uuid = d.load_xclbin(xbin)
 
-    COUNT = 1024*16
-    DATA_SIZE = ctypes.sizeof(ctypes.c_int32) * COUNT
+    kernellist = xbin.get_kernels()
 
-    # Instantiate vadd
-    kHandle = pyxrt.kernel(d, uuid, "vadd", pyxrt.kernel.shared)
+    rule = re.compile("vadd*")
+    kernel = list(filter(lambda val: rule.match(val.get_name()), kernellist))[0]
+    kHandle= pyxrt.kernel(d, uuid, kernel.get_name(), pyxrt.kernel.shared)
 
+    zeros = bytearray(opt.DATA_SIZE)
     print("Allocate and initialize buffers")
-    boHandle1 = pyxrt.bo(d, DATA_SIZE, pyxrt.bo.normal, kHandle.group_id(0))
-    boHandle2 = pyxrt.bo(d, DATA_SIZE, pyxrt.bo.normal, kHandle.group_id(1))
-    boHandle3 = pyxrt.bo(d, DATA_SIZE, pyxrt.bo.normal, kHandle.group_id(2))
-    bo1 = numpy.asarray(boHandle1.map())
-    bo2 = numpy.asarray(boHandle2.map())
-    bo3 = numpy.asarray(boHandle3.map())
+    boHandle1 = pyxrt.bo(d, opt.DATA_SIZE, pyxrt.bo.normal, kHandle.group_id(0))
+    boHandle1.write(zeros, 0)
+    bo1 = boHandle1.map()
 
-    for i in range(COUNT):
+    boHandle2 = pyxrt.bo(d, opt.DATA_SIZE, pyxrt.bo.normal, kHandle.group_id(1))
+    boHandle2.write(zeros, 0)
+    bo2 = boHandle2.map()
+
+    boHandle3 = pyxrt.bo(d, opt.DATA_SIZE, pyxrt.bo.normal, kHandle.group_id(2))
+    boHandle3.write(zeros, 0)
+    bo3 = boHandle3.map()
+
+    for i in range(opt.DATA_SIZE):
         bo1[i] = 1 
         bo2[i] = 2
 
-    bufReference = [3 for i in range(COUNT)]
+    bufReference = [3 for i in range(opt.DATA_SIZE)]
 
-    boHandle1.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE, DATA_SIZE, 0)
-    boHandle2.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE, DATA_SIZE, 0)
+    boHandle1.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE, opt.DATA_SIZE, 0)
+    boHandle2.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE, opt.DATA_SIZE, 0)
 
     print("Start the kernel")
-    run = kHandle(boHandle1, boHandle2, boHandle3, COUNT)
+    run = kHandle(boHandle1, boHandle2, boHandle3, opt.DATA_SIZE)
     print("Now wait for the kernel to finish")
     state = run.wait()
 
     print("Get the output data from the device and validate it")
-    boHandle3.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_FROM_DEVICE, DATA_SIZE, 0)
+    boHandle3.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_FROM_DEVICE, opt.DATA_SIZE, 0)
     
-    for i in range(COUNT):
+    for i in range(opt.DATA_SIZE):
         if(bufReference[i] != bo3[i]):
             print("Computed value done not match reference")
             assert False
