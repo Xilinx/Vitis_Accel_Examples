3f19eed0a09996417cdb81c4324ee00f3c19be1f
Bhaskar VishnuVardhan Chebrolu <bhaskar.vishnuvardhan.chebrolu@xilinx.com>
2022-08-03 20:55:15 +0530

    disabling kernel swap design for sw_emu & hw_emu when targeting vck190_base_dfx platform

--- sys_opt/kernel_swap/config.mk
+++ sys_opt/kernel_swap/config.mk
@@ -1,3 +1,8 @@
 ifeq ($(findstring dfx, $(PLATFORM)), dfx)
 CXXFLAGS += -Ddfx_device
 endif
+ifeq ($(TARGET),$(filter $(TARGET),sw_emu hw_emu))
+ifeq ($(findstring vck190_base_dfx, $(PLATFORM)), vck190_base_dfx)
+$(error [ERROR]: This example is not supported for sw_emu & hw_emu when targeting $(PLATFORM).)
+endif
+endif
