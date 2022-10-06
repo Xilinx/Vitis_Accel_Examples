fe4c5ba9290fa959825dc933456f49f70f34029d
Sonal Agarwal <2220+sonala@users.noreply.gitenterprise.xilinx.com>
2022-10-07 00:24:25 +0530

    updated config.mk to not run hw_emu zcu102_base_dfx platform (#1405)

--- sys_opt/kernel_swap/config.mk
+++ sys_opt/kernel_swap/config.mk
@@ -2,7 +2,13 @@ ifeq ($(findstring dfx, $(PLATFORM)), dfx)
 CXXFLAGS += -Ddfx_device
 endif
 ifeq ($(TARGET),$(filter $(TARGET),sw_emu hw_emu))
-ifeq ($(findstring vck190_base_dfx, $(PLATFORM)), vck190_base_dfx)
+ifeq ($(findstring vck190_base_dfx, $(PLATFORM)),vck190_base_dfx)
 $(error [ERROR]: This example is not supported for sw_emu & hw_emu when targeting $(PLATFORM).)
 endif
 endif
+ifeq ($(TARGET),$(filter $(TARGET) ,hw_emu))
+ifeq ($(findstring zcu102_base_dfx, $(PLATFORM)), zcu102_base_dfx)
+$(error [ERROR]: This example is not supported for hw_emu when targeting $(PLATFORM).)
+endif
+endif
+
