cc3c67d5c93933f645e6a6edf54891b682044fb5
Meghana Tripathi <meghanat@xilinx.com>
2023-04-19 21:40:00 +0530

    disable sw_emu for mailbox_xrt

--- host_xrt/mailbox_auto_restart_xrt/makefile_zynqmp.mk
+++ host_xrt/mailbox_auto_restart_xrt/makefile_zynqmp.mk
@@ -178,6 +178,10 @@ else
 	$(ECHO) "Please copy the content of sd_card folder and data to an SD Card and run on the board"
 endif
 
+ifneq ($(TARGET),$(findstring $(TARGET), hw hw_emu))
+$(error Application supports only hw hw_emu TARGET. Please use the target for running the application)
+endif
+
 .PHONY: test
 test: $(EXECUTABLE)
 ifeq ($(TARGET),$(filter $(TARGET),sw_emu hw_emu))
